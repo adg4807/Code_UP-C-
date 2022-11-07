681746e13d5e34bb37a73399edc412c4268a06d4
adg4807 <81452897+adg4807@users.noreply.github.com>
2022-11-07 16:59:55 +0900

    Update codeup(1430).c

--- codeup(1430).c
+++ codeup(1430).c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
+int arr_n[10000001]={0};
+
 int main(void)
 {
 	int n, m;
-	int arr_n[10000000];
 	int num;
 
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++)
 	{
-
 		scanf("%d", &num);
 		arr_n[num] = 1;
 	}
-
 	scanf("%d", &m);
 	for (int i = 0; i < m; i++)
 	{
-
 		scanf("%d", &num);
-		printf("%d", arr_n[num]);
+		printf("%d ", arr_n[num]);
 	}
-
-
 	return 0;
 }
